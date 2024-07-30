c4e017e6f995a60fc7d2a232158d168f80393585
himan07 <himan9714@gmail.com>
2024-07-30 12:54:15 +0530

    leet code remove element questions solved

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 // import ArrayTraverse from "./files/ArrayTraverse";
 // import InsertElArr from "./files/InsertElArr";
-// import DelElement from "./files/DelElement";
-import SearchEl from "./files/SearchEl";
+import DelElement from "./files/DelElement";
+// import SearchEl from "./files/SearchEl";
 
 const App = () => {
   return (
     <div style={{ textAlign: "center", color: "#fff" }}>
      {/* <ArrayTraverse /> */}
      {/* <InsertElArr /> */}
-     {/* <DelElement /> */}
-     <SearchEl />
+     <DelElement />
+     {/* <SearchEl /> */}
     </div>
   );
 };
--- src/files/DelElement.jsx
+++ src/files/DelElement.jsx
@@ -6,7 +6,7 @@ const DelElement = () => {
   let data = [2, 5, 6, 80, 20, 40, 90];
 
   const handleClick = () => {
-    let position = parseInt(positions)
+    let position = parseInt(positions);
     for (let i = position; i < data.length - 1; i++) {
       data[i] = data[i + 1];
     }
@@ -14,14 +14,49 @@ const DelElement = () => {
     console.log("transformed arr; ", data);
   };
 
+  function removeEle(nums, val) {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== val) {
+        nums[k] = nums[i];
+        k++;
+      }
+    }
+
+    nums.length = nums.length - 1;
+    console.log(nums);
+    return k;
+  }
+
+  let arr = [1, 2, 3, 5, 8, 7, 9];
+  removeEle(arr, 7);
+
+  // or another approach to solve this problem
+
+  function removeElement(nums, val) {
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== val) {
+        nums[index] = nums[i];
+        index++;
+      }
+      nums.length = index;
+      return nums;
+    }
+    console.log(nums)
+  }
+
+  removeElement(arr, 9);
+
   return (
     <div>
       <input
         type="text"
         placeholder="enter the position to remove the element! "
         onChange={(e) => setPositions(e.target.value)}
-        style={{width:"300px"}}
-      />&nbsp;
+        style={{ width: "300px" }}
+      />
+      &nbsp;
       <button onClick={handleClick}> Remove Element</button>
     </div>
   );

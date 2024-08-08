775da3ab2de248811bd455e565c1a81bf4920ba9
Himanshu yadav <himanshuyadav@Himanshus-Laptop.local>
2024-08-08 21:53:56 +0530

    questions based on array

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import React from "react";
-// import ArrayTraverse from "./files/ArrayTraverse";
-// import InsertElArr from "./files/InsertElArr";
-// import DelElement from "./files/DelElement";
-// import SearchEl from "./files/SearchEl";
-import MeregeTwoArray from "./files/MeregeTwoArray";
+import React from 'react'
 
 const App = () => {
   return (
-    <div style={{ textAlign: "center", color: "#fff" }}>
-     {/* <ArrayTraverse /> */}
-     {/* <InsertElArr /> */}
-     {/* <DelElement /> */}
-     {/* <SearchEl /> */}
-     <MeregeTwoArray />
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App

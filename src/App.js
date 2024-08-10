a4487fd3163d945ffc56f37fc8225169ebf8b1d4
Himanshu yadav <himanshuyadav@Himanshus-Laptop.local>
2024-08-10 18:15:40 +0530

    problem solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import ArrayTraverse from "./files/ArrayTraverse";
+import DelElement from "./files/DelElement";
+import MeregeTwoArray from "./files/MeregeTwoArray.jsx";
+import SearchEl from "./files/SearchEl.jsx";
+import InsertElArr from "./files/InsertElArr.jsx";
+import Problem from "./files/Problem.jsx";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      {/* <ArrayTraverse /> */}
+      {/* <DelElement /> */}
+      <InsertElArr />
+      {/* <MeregeTwoArray /> */}
+      {/* <SearchEl /> */}
+      {/* <Problem /> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/files/InsertElArr.jsx
+++ src/files/InsertElArr.jsx
@@ -1,56 +1,68 @@
-import React, { useState } from "react";
+import React from "react";
 
 const InsertElArr = () => {
-  const [positions, setPositions] = useState();
-  const [elements, setElements] = useState();
-
-  let data = [60, 30, 10, 67, 40];
-
-  // const handleClick = () => {
-  //   for (let i = data.length - 1; i >= 0; i--) {
-  //     if (i >= positions) {
-  //       data[i + 1] = data[i];
-  //       if (i == positions) {
-  //         data[i] = parseInt(elements);
-  //       }
-  //     }
-  //   }
-  //   console.log("dynamic data: ", data);
-  // };
-
-  const handleClick = () => {
-    for (let i = data.length - 1; i >= 0; i--) {
-      if (i >= positions) {
-        data[i + 1] = data[i];
-        if (i == positions) {
-          data[i] = parseInt(elements);
+  function InsertElement(arr) {
+    let newEl = 5;
+    let poisition = 3;
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (i >= poisition) {
+        arr[i + 1] = arr[i];
+        if (i === poisition) {
+          arr[i] = newEl;
         }
       }
     }
-    console.log("modified array: ", data);
+    // console.log(arr);
+  }
+  let arr = [10, 30, 20, 50, 60, 90];
+  InsertElement(arr);
+
+  // Problem: Given an array [2, 4, 6, 8], insert the element 10 at the end of the array.
+  function InsertElAtEnd(array) {
+    let lastIndex = array.length;
+    let newNo = 10;
+    array[lastIndex] = newNo;
+    // console.log(array);
+  }
+  let data = [2, 4, 6, 8];
+  InsertElAtEnd(data);
+
+  // Problem: Given a sorted array [1, 3, 5, 7], insert the element 4 so that the array remains sorted.
+
+  function InserElSorted(array) {
+    let element = 4;
+    let position = 2;
+    for (let i = array.length - 1; i >= 0; i--) {
+      if (i >= position) {
+        array[i + 1] = array[i];
+        if (i == position) {
+          array[i] = element;
+        }
+      }
+    }
+    // console.log("inserted element in the sorted array: ",array);
+  }
+  let newarr = [1, 3, 5, 7];
+  InserElSorted(newarr);
+
+  // Problem: Given an array [10, 20, 30, 50], insert the elements [35, 40, 45] at index 3.
+
+  const InsertMultipleEl = (arr) => {
+    let position = 3;
+    let elements = [35, 40, 45];
+    for (let i = arr.length - 1; i >= position; i--) {
+      arr[i + elements.length] = arr[i];
+    }
+    for (let j = 0; j < elements.length; j++) {
+      arr[position + j] = elements[j];
+    }
+    console.log("multiple elements :", arr);
   };
 
-  
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter New Position"
-        onChange={(e) => setPositions(e.target.value)}
-      />
-      <br />
-      <br />
-      <input
-        type="text"
-        placeholder="Enter New Element"
-        onChange={(e) => setElements(e.target.value)}
-      />
-      <br />
-      <br />
+  let datas = [10, 20, 30, 50];
+  InsertMultipleEl(datas);
 
-      <button onClick={handleClick}>Insert</button>
-    </div>
-  );
+  return <div>InsertElArr</div>;
 };
 
 export default InsertElArr;

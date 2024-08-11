75e71a5484ef6a53f12d5a2c48b902a1f8ed5e12
Himanshu yadav <himanshuyadav@Himanshus-Laptop.local>
2024-08-11 10:55:25 +0530

    problem solved

--- src/files/InsertElArr.jsx
+++ src/files/InsertElArr.jsx
@@ -56,11 +56,73 @@ const InsertElArr = () => {
     for (let j = 0; j < elements.length; j++) {
       arr[position + j] = elements[j];
     }
-    console.log("multiple elements :", arr);
+    // console.log("multiple elements :", arr);
   };
 
   let datas = [10, 20, 30, 50];
   InsertMultipleEl(datas);
+
+  // Problem: Given an array [2, 4, 6, 9, 11, 15], insert the element 8 such that all even numbers come before all odd numbers.
+
+  function InserElBySeries(arr, elements) {
+    let even = [];
+    let odd = [];
+    let isEven = elements % 2 === 0;
+
+    for (let num of arr) {
+      if (num % 2 === 0) {
+        even.push(num);
+      } else {
+        odd.push(num);
+      }
+    }
+
+    if (isEven) {
+      even.push(elements);
+    } else {
+      odd.push(elements);
+    }
+
+    let result = even.concat(odd);
+
+    console.log("this is shorted array", result);
+  }
+
+  let newArr = [2, 4, 6, 9, 11, 15];
+  InserElBySeries(newArr, 8);
+
+  // another method
+
+  const insertElementMaintainingOrder = (arr, element) => {
+    let isElementEven = element % 2 === 0;
+    let position = arr.length;
+
+    for (let i = 0; i < arr.length; i++) {
+      let currentIsEven = arr[i] % 2 === 0;
+      if (
+        (isElementEven && !currentIsEven) ||
+        (!isElementEven && currentIsEven)
+      ) {
+        position = i;
+        break;
+      }
+    }
+
+    let newArr = new Array(arr.length + 1);
+
+    for (let i = 0; i < position; i++) {
+      newArr[i] = arr[i];
+    }
+    newArr[position] = element;
+
+    for (let i = position; i < arr.length; i++) {
+      newArr[i + 1] = arr[i];
+    }
+
+    console.log("updated array: ", newArr);
+  };
+
+  insertElementMaintainingOrder(newArr, 8)
 
   return <div>InsertElArr</div>;
 };

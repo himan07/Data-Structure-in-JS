dfc9f23dc16cfab2df2e239572469d28f8a1eded
Himanshu yadav <himanshuyadav@Himanshus-Laptop.local>
2024-08-12 23:10:25 +0530

    some problem solved

--- src/files/SearchEl.jsx
+++ src/files/SearchEl.jsx
@@ -44,6 +44,49 @@ Output: The index of the target value in the array.
   let array = [4, 2, 7, 4, 9, 4, 1];
   findOccurances(array, 4);
 
+  /*
+  Problem 2: Search for First Occurrence
+
+Array: [9, 14, 15, 21, 15, 30, 40]
+Target Value: 15
+Output: The index of the first occurrence of the target value. If not found, return -1.
+  */
+
+  function findFirstIndex(arr, element) {
+    let index = -1;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === element) {
+        index = i;
+        break;
+      }
+    }
+    console.log("resultedIndex: ", index);
+  }
+  let arry = [9, 14, 15, 21, 15, 30, 40];
+  findFirstIndex(arry, 15);
+
+  /*
+Problem 3: Search for Last Occurrence
+
+Array: [9, 14, 15, 21, 15, 30, 15]
+Target Value: 15
+Output: The index of the last occurrence of the target value. If not found, return -1.
+*/
+
+  function findLastOccurances(arr, ele) {
+    let index = undefined;
+    for (let i = arr.length-1 ; i >= 0; i--) {
+      if (arr[i] === ele) {
+        index = i;
+        break;
+      } else {
+        index = -1;
+      }
+    }
+    console.log("lastOccurance", index);
+  }
+
+  findLastOccurances([9, 14, 15, 21, 15, 30, 15], 15);
   return <div></div>;
 };
 

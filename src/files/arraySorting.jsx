4c1899d3839ebe51149ce7f02afd0a044d559929
himan07 <himan9714@gmail.com>
2024-08-30 09:37:17 +0530

    changes

--- src/files/arraySorting.jsx
+++ src/files/arraySorting.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const ArraySorting = () => {
-  return (
-    <div>arraySorting</div>
-  )
-}
+  // destructuring the elements from arrays
 
-export default ArraySorting
+  const arr = [2, 3, 4];
+  const [x, y, z] = arr;
+  console.log("destructured elements: ", x, y, z);
+  const restaurent = {
+    name: "Classino Italino",
+    location: "India, France, China, Itally",
+    categories: ["Itallian", "Pizzaria", "Vegetarian", "Organic"],
+    starterMenu: ["Foccacia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+    mainMenu: ["Pizza", "Pasta", "Risotto"],
+  };
+
+  let [main, secondary] = restaurent.categories;
+
+ [main, secondary] = [secondary, main]
+
+  // switching the elements
+
+  // const temp = main;
+  // main = secondary;
+  // secondary = temp;
+  console.log("switched element: ",main, secondary)
+
+  return <div>arraySorting</div>;
+};
+
+export default ArraySorting;

ca71c84f9a057355e7da2cfb8f7d6df07709c652
himan07 <himan9714@gmail.com>
2024-09-09 20:39:35 +0530

    merged two sorted array done

--- src/files/arraySorting.jsx
+++ src/files/arraySorting.jsx
@@ -40,7 +40,28 @@ const ArraySorting = () => {
     let i = 0;
     let j = 0;
     let k = 0;
-    
+
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        arr3[k] = arr1[i];
+        i++;
+      } else {
+        arr3[k] = arr2[j];
+        j++;
+      }
+      k++;
+    }
+
+    while (i < arr1.length) {
+      arr3[k] = arr1[i];
+      i++;
+      k++;
+    }
+    while (j < arr2.length) {
+      arr3[k] = arr2[j];
+      j++;
+      k++;
+    }
 
     console.log("mergedArr: ", arr3);
   };

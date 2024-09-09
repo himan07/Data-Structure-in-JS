dab35564a42a041ac28dabb5fa22db30fb3db1b8
himan07 <himan9714@gmail.com>
2024-09-09 20:09:14 +0530

    update

--- src/files/arraySorting.jsx
+++ src/files/arraySorting.jsx
@@ -14,9 +14,25 @@ const ArraySorting = () => {
     }
     console.log("sorted value: ", arr);
   };
-
   sortedArry([40, 30, 12, 25]);
-  
+
+  // sort this array in ascending order const arr = [34, 7, 23, 32, 5, 62];
+  const sortArrAsc = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > arr[j + 1]) {
+          let temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    console.log("sorted asc value: ", arr);
+  };
+
+  sortArrAsc([34, 7, 23, 32, 5, 62])
+
+
   return <div>arraySorting</div>;
 };
 
